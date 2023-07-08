e936453cfd20da6f3e4a60407cb6e9af1ba7c4ff
kurousa <r.takahashi.1026@gmail.com>
2023-07-08 16:13:45 +0900

    add routeing object '/api'

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const api = require('./routes/api');
+
 const app = express();
 const port = 3000;
 
 // specify destination of static files.
 app.use(express.static("public"));
+// specify routing object to route '/api'
+app.use("/api", api);
 
 app.get("/", (req, res, next) => {
     res.end("Top Page");
