971d10c08cb95f694f1979d279fb0efb72539de9
kurousa <r.takahashi.1026@gmail.com>
2023-07-08 15:52:54 +0900

    add static html and published on express.

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// specify destination of static files.
+app.use(express.static("public"));
+
 app.get("/", (req, res, next) => {
     res.end("Top Page");
 });
